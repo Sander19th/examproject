9430b3bbd82c30302ced5a944a97684ecb9bb341
Alexander Bartram Laursen <alexander@vildmedtog.dk>
2023-12-10 21:04:26 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1> Plantuary</h1>,
+  document.getElementById("root")
+);
+
+let element = <img id="Exotic leaves" className="Exotic Leaves" src="examproject/Images/Exotic_Leaves"></img>
