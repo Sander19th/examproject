3aa0b8ebe220938ff9d6cb2c42b84385a119d974
Alexander Bartram Laursen <alexander@vildmedtog.dk>
2023-12-12 01:20:17 +0100

    Added images and titles for plants

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let element = (
+  <>
+      <h1>Plantuary</h1>
+      <div></div>
+
+      <div id="Homepagepilea"><img src="./images/Pilea_pot.png" alt=""></img></div>
+      <h3>Pilea Peperomioides</h3>
+      <b>Also known as the chinese money plant for it's iconic rounded leaves..</b>
+
+      <div id="Homepagemonstera"><img src="./images/Monstera_pot.png" alt=""></img></div>
+      <h3>Monstera Deliciosa</h3>
+      <b>Commonly reffered to as the swiss cheese plant due to it's blossomed leaves having holes..</b>
+
+      <div id="Homepagecocos"><img src="./images/Cocos_pot.jpg" alt=""></img></div>
+      <h3>Cocos Nucifera</h3>
+      <b>Lorum ipsum</b>
+      <div id="Homepageimage"><img src="./images/Exotic_Leaves.jpg" alt=""></img></div>
+  </>
+)
 
 ReactDOM.render(
-  <h1><i>Plantuary</i></h1>,
+  element,
   document.getElementById("root")
 );
+
 
