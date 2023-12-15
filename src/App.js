5daa400f5784362ad5344bb5ffc7028c38a5eadd
Alexander Bartram Laursen <alexander@vildmedtog.dk>
2023-12-15 01:39:08 +0100

    Added a search function in JS

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import "./index.html";
+import React from "react";
+
+/*
+function App() {
+    return (
+            <div className="App">
+                <input type="text" placeholder="Search for plant..." />
+            </div>
+    );
+}
+
+*/
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
 let element = (
   <>
+    
   </>
 )
+
 
 ReactDOM.render(
   element,
   document.getElementById("root")
 );
+*/
 
-function MainMenu(props) {
+  document.getElementById('search').addEventListener('input', function() {
+   
+    var searchText = this.value.toLowerCase();
 
-} 
+   
+    var plantItems = document.querySelectorAll('.plant');
+
+   
+    plantItems.forEach(function(item) {
+      
+      var plantText = item.textContent.toLowerCase();
+
+      if (plantText.includes(searchText)) {
+        
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
